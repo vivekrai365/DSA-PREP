379afc1de0e295b6efed7c64e349adbc3fa78f2c
vivekrai365 <vivekcvrce@gmail.com>
2025-06-10 19:18:18 +0530

    added Proper comments to ReverseOfElement.java and SumOfElements.java

--- Dsa_Prep/src/dataStructure/ArraysConcept/ReverseOfElements.java
+++ Dsa_Prep/src/dataStructure/ArraysConcept/ReverseOfElements.java
@@ -6,12 +6,26 @@ public class ReverseOfElements {
 
 	public static void main(String[] args) {
 		/*
-		 * In this code block a Array is reversed and stored in a a different  Array and
-		 * then printed
+		 * In this code, we are reversing the elements of an integer array.
+		 * 
+		 * The original array `arr` contains elements from 0 to 9.
+		 * We create a new array `reverseArr` of the same length to store
+		 * the reversed elements of `arr`.
 		 */
 		
 		int arr[]= {0,1,2,3,4,5,6,7,8,9};
 		int reverseArr[]=new int[arr.length];
+		
+		/*
+		 * This loop runs from 0 to arr.length - 1.
+		 * For each index i, we assign the value from the end of `arr`
+		 * to the corresponding index in `reverseArr`.
+		 * 
+		 * For example:
+		 * i = 0  => reverseArr[0] = arr[9]
+		 * i = 1  => reverseArr[1] = arr[8]
+		 * ...
+		 */
 		
 		for (int i=0;i<arr.length-1;i++) 
 		{
@@ -19,6 +33,11 @@ public class ReverseOfElements {
 		}
 		
 		System.out.println(Arrays.toString(reverseArr));
+		
+		/*
+		 * Arrays.toString() is used to print the array in a readable format.
+		 * It calls toString() internally to display each element in order.
+		 */
 	}
 
 }
--- Dsa_Prep/src/dataStructure/ArraysConcept/SumOfElements.java
+++ Dsa_Prep/src/dataStructure/ArraysConcept/SumOfElements.java
@@ -3,13 +3,34 @@ package dataStructure.ArraysConcept;
 public class SumOfElements {
 
 	public static void main(String[] args) {
-		int []arr= {1,2,3,4,5,6,7,8,9};
-		int sum=0;
+		/*
+		 * In this program, we are calculating the sum of all elements
+		 * in an integer array using a simple for-loop.
+		 */
+		
+		int []arr= {1,2,3,4,5,6,7,8,9}; // Declaration and initialization of the array
+		int sum=0;// Variable to hold the cumulative sum of array elements
+		
+		/*
+		 * Iterate through each element of the array using a for loop.
+		 * For every iteration, add the current element (arr[i]) to the sum.
+		 * 
+		 * For example:
+		 * i = 0  => sum = 0 + arr[0] = 1
+		 * i = 1  => sum = 1 + arr[1] = 3
+		 * ...
+		 */
 		
 		for(int i=0;i<arr.length;i++) 
 		{
 			 sum=sum+arr[i];
 		}
+		
+		/*
+		 * After the loop completes, the variable `sum` contains the total
+		 * of all elements in the array. We then print the result.
+		 */
+		
 		System.out.println("The sum of the elements of the Array(arr) is "+sum);
 	}
 
